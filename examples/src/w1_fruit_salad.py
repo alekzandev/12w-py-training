8a8b246a664a8ec9498f8421f0ecf613cbdb0e6d
alekzandev <aleksandev@proton.me>
2024-12-17 22:43:23 -0500

    add example files

---
+++ examples/src/w1_fruit_salad.py
@@ -0,0 +1,28 @@
+import click
+import logging
+from click.testing import CliRunner
+
+logging.basicConfig(level=logging.INFO)
+
+@click.command()
+@click.option('--fruits')
+def salad_mixer(fruits):
+    """
+    Mix fruits to make a fruit salad.
+
+    :param fruits: list of fruits
+    :return: fruit_salad
+    """
+    if not fruits:
+        logging.error('No fruits to mix!')
+        return 'No Salad! :c'
+    else:
+        fruits = eval(fruits)
+    logging.info(f'Mixing fruits: {fruits}')
+    fruit_salad = ', '.join(fruits)
+    logging.info(f'Fruit salad ready: {fruit_salad}')
+    return fruit_salad
+
+if __name__ == '__main__':
+    print(salad_mixer(standalone_mode=False))
+    
---
+++ examples/tests/test_w1_fruit_salad.py
@@ -0,0 +1,14 @@
+from click.testing import CliRunner
+from src.w1_fruit_salad import salad_mixer
+
+def test_salad_mixer_no_fruits():
+    runner = CliRunner()
+    result = runner.invoke(salad_mixer, [])
+    assert result.exit_code == 0
+    #assert 'apple, banana, orange' in result.output
+
+def test_salad_mixer_with_fruits():
+    runner = CliRunner()
+    result = runner.invoke(salad_mixer, ['["apple", "banana", "cherry"]'])
+    assert result.exit_code == 2
+    #assert 'apple, banana, cherry' in result.output
